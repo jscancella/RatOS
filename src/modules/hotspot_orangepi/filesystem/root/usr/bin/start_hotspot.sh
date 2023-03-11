b5dd2ea776e95d0794546a7b4576b274f3baef57
John Scancella <blacksmithforlife@gmail.com>
2023-03-10 19:26:07 -0500

    fix: startup wifi after configurator sets ssid and password

---
+++ src/modules/hotspot_orangepi/filesystem/root/usr/bin/start_hotspot.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [[ -e /etc/wpa_supplicant/wpa_supplicant.conf ]]
+then
+  #get info from wpa_supplicant
+  SSID=`grep -E "^\s+ssid" /etc/wpa_supplicant/wpa_supplicant.conf | sed 's/^[[:space:]]*//g' | cut -c 7- | sed 's/.$//'`
+  #PASSWORD=`grep -E "^\s+#psk" /etc/wpa_supplicant/wpa_supplicant.conf | sed 's/^[[:space:]]*//g' | cut -c 6-`
+  PASSWORD=`grep -E "^\s+psk" /etc/wpa_supplicant/wpa_supplicant.conf | sed 's/^[[:space:]]*//g' | cut -c 5-`
+  sudo nmcli dev wifi connect ${SSID} password ${PASSWORD}
+fi
+
+#if wifi is not connected
+ip link show dev wlan0 | grep -q "state UP"
+check=$?
+if [ "$check" -ne 0 ]
+then
+        /usr/bin/create_ap --config /etc/create_ap.conf
+fi
