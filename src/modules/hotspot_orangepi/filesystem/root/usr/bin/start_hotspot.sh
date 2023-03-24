3893f09753c8c631dacb0418317d1780cd2ae4a6
John Scancella <blacksmithforlife@gmail.com>
2023-03-24 14:36:06 -0400

    feat: add more log information when starting hotspot and wifi

--- src/modules/hotspot_orangepi/filesystem/root/usr/bin/start_hotspot.sh
+++ src/modules/hotspot_orangepi/filesystem/root/usr/bin/start_hotspot.sh
@@ -6,7 +6,10 @@ then
   SSID=`grep -E "^\s+ssid" /etc/wpa_supplicant/wpa_supplicant.conf | sed 's/^[[:space:]]*//g' | cut -c 7- | sed 's/.$//'`
   #PASSWORD=`grep -E "^\s+#psk" /etc/wpa_supplicant/wpa_supplicant.conf | sed 's/^[[:space:]]*//g' | cut -c 6-`
   PASSWORD=`grep -E "^\s+psk" /etc/wpa_supplicant/wpa_supplicant.conf | sed 's/^[[:space:]]*//g' | cut -c 5-`
-  nmcli dev wifi connect "${SSID}" password "${PASSWORD}"
+  #TODO logrotate
+  "preparing to connect to wifi with SSID: '${SSID}' and password: '${PASSWORD}'" >> /var/log/vcore.log
+  nmcli dev wifi connect "${SSID}" password "${PASSWORD}" 2>&1 >> /var/log/vcore.log
+  sleep 15
 fi
 
 #if wifi is not connected
@@ -14,5 +17,6 @@ ip link show dev wlan0 | grep -q "state UP"
 check=$?
 if [ "$check" -ne 0 ]
 then
-        /usr/bin/create_ap --config /etc/create_ap.conf
+  "starting RatOS hotspot" >> /var/log/vcore.log
+  /usr/bin/create_ap --config /etc/create_ap.conf 2>&1 >> /var/log/vcore.log
 fi
